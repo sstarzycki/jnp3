29af07f8baced15747801de72c27ebc5266b08c2
Szymon Starzycki <simonstar@wp.pl>
2011-12-15 11:40:01 +0100

    Empty upload

--- paste/text/views.py
+++ paste/text/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from text.models import Text
 
 
 def show_main(request):
 
     return render('base.html', {})
+
+def new(request):
+    return render_to_response('paste/new.html', {});
+
