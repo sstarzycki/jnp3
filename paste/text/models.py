dca80191ed479e38ddfe8a3b64cfd4aeb38f28e3
Jakub Tlałka <mamba57@gmail.com>
2011-12-15 11:55:17 +0100

    changes

--- paste/text/models.py
+++ paste/text/models.py
@@ -5,5 +5,5 @@ class Text(models.Model):
     user = models.ForeignKey(User)
     content = models.TextField(max_length=255)
     description = models.CharField(max_length=255)
-    upload_date = models.DateTimeField()
+    upload_date = models.DateTimeField(null=True)
 # Create your models here.
