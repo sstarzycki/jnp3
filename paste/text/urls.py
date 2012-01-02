1fee70588ccb7d7e3b67be7131d5a916a69d2d44
Szymon Starzycki <ss292455@students.mimuw.edu.pl>
2012-01-02 21:14:12 +0100

    urls change

--- paste/text/urls.py
+++ paste/text/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns("text.views",
     url(r'^new/', 'new'),
+    url(r'^create/', 'create'),
+    url(r'^show/(?P<paste_id>\d+)/', 'show'),
 )
