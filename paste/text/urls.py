93c86ae7fbae44c86f41d3f149e3a42a93e0a074
Szymon Starzycki <ss292455@students.mimuw.edu.pl>
2012-01-04 20:41:23 +0100

    show url changed

--- paste/text/urls.py
+++ paste/text/urls.py
@@ -4,5 +4,5 @@ from text.views import NewTextView, ListTextView, ShowTextView
 urlpatterns = patterns("text.views",
     (r'^new/$', NewTextView.as_view()),
     (r'^list/$', ListTextView.as_view()),
-    (r'^show/(?P<pk>[a-z0-9]+)$', ShowTextView.as_view()),
+    (r'^show/(?P<pk>[a-z0-9]+)/$', ShowTextView.as_view()),
 )
