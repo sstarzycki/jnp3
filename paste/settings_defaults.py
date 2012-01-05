b0b098cf7b1335d3a7fd6aed172df4195dc07ff3
Jakub Tlałka <mamba57@gmail.com>
2012-01-05 10:46:55 +0100

    restored database

--- paste/settings_defaults.py
+++ paste/settings_defaults.py
@@ -22,6 +22,17 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pastedb',                      # Or path to database file if using sqlite3.
+        'USER': 'paster',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 
 mongoengine.connect('paste')
 
